23094138d8f61aff7c70c722800dc19154a25a4b
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-27 22:14:25 -0700

    required files

---
+++ caps.js
@@ -0,0 +1,8 @@
+// like brain.js file
+// require files: driver, vendor, events
+// manages the stage of shipment/delivery of products
+
+const driver = require('./driver/driver.js');
+const vendor = require('./vendor/vendor.js');
+const events = require('./events.js');
+
