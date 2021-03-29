5bbdd6fd6033ff0b534062a94766b6c162cbf415
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-29 16:05:41 -0700

    wrote tests

--- __tests__/events.test.js
+++ __tests__/events.test.js
@@ -1,15 +1,48 @@
 'use strict';
 
-// need to require these files to test their logs
+const caps = require('../caps.js');
+
 const driver = require('../driver/driver.js');
+
+const faker = require('faker');
+
 const vendor = require('../vendor/vendor.js');
 
-// vendor: order ready
+const order = new vendor.Vendor();
 
-// driver: order picked up
+function timestamp() {
+  return new Date().toDateString();
+}
 
-// driver: en route
+let testPayload = {
+  event: 'pickup',
+  time: timestamp(),
+  payload: {
+    storeName: 'Garden Galore',
+    customerName: faker.name.findName(),
+    timeOfPurchase: timestamp(),
+    shippingAddress: `${faker.address.streetAddress('###')}, ${faker.address.city()}, ${faker.address.stateAbbr} ${faker.address.zipCode('#####')}`,
+    orderId: faker.datatype.uuid() 
+  }
+}
 
-// driver: status - delivered
+console.log = jest.fn();
 
-// vendor: thank you message
+describe('====== Communications Test ======', () => {
+  it('vendor notifies driver that order is ready for pickup', () => {
+    let test = caps.createOrder();
+
+    expect(console.log).toHaveBeenCalled();
+    clearInterval(test);
+  });
+
+  it('driver notifies vendor that order is in-transit', () => {
+    driver.orderPickedUp(testPayload);
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('driver notifies vendor that order is delivered', () => {
+    driver.orderDelivered(testPayload);
+    expect(console.log).toHaveBeenCalled();
+  });
+});
